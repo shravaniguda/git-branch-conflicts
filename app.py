570ace6f2072a36d45fc1f35a11b41ccf6ebb5bc
Shravani Guda <shravaniguda.15@gmail.com>
2026-05-28 00:58:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import boto3
 from botocore.exceptions import ClientError
 
 # Create S3 client
-s3 = boto3.client('rds')
+s3 = boto3.client('lambda')
 def list_buckets():
     try:
         response = s3.list_buckets()
